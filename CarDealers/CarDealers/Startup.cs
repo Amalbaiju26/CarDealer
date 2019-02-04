dc96a7451011578f88610aea01c37b7efde82d84
Amal Baiju <Amal Baiju@DESKTOP-M1ALJPQ>
2019-02-04 16:19:46 -0500

    First Commit

---
+++ CarDealers/CarDealers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarDealers.Startup))]
+namespace CarDealers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
